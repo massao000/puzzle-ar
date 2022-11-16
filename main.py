2131be95a09d9c17463bb782730c4bd3ce627448
massao000 <69783019+massao000@users.noreply.github.com>
2022-11-16 15:04:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ import random
 
 from package import *
 
+st.set_page_config(
+    page_title="AR puzzle",
+    page_icon="🧊",
+    layout="wide"
+)
+
 aruco = cv2.aruco
 # https://elsammit-beginnerblg.hatenablog.com/entry/2020/10/10/125246
 # https://amdlaboratory.com/amdblog/opencv%E3%81%A8aruco%E3%83%9E%E3%83%BC%E3%82%AB%E3%83%BC%E3%82%92%E5%88%A9%E7%94%A8%E3%81%97%E3%81%9F%E7%94%BB%E5%83%8F%E3%83%9E%E3%83%83%E3%83%94%E3%83%B3%E3%82%B0/
@@ -71,18 +77,6 @@ webrtc_streamer(
     video_frame_callback=video_frame_callback,
     rtc_configuration={  # Add this config
         "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
-    }
-)
-
-st.set_page_config(
-    page_title="AR puzzle",
-    page_icon="🧊",
-    layout="wide",
-    initial_sidebar_state="expanded",
-    menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "# This is a header. This is an *extremely* cool app!"
     }
 )
 
