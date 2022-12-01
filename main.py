4f10f114726d78ead5339e2bc8dd9dbc5f39549b
massao000 <onepiece.4224y@icloud.com>
2022-12-01 23:30:09 +0900

    サイズの追加 画像抽選単発だけに変更

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ video = VideoProcessor()
 is_random_img = False
 
 option = st.sidebar.selectbox(
-    label = "パズルサイズ",
-    options = ["2x2", "3x3", "4x4", "5x5", "3x2", "4x3", "5x4", "2x3"]
+    label = "パズルサイズ 横x縦",
+    options = ["2x2", "3x3", "4x4", "5x5", "3x2", "4x3", "5x4", "2x3", "3x4"]
 )
 
 if option == "2x2":
@@ -45,69 +45,74 @@ elif option == "3x2": # 横長 2:1
     video.rows, video.cols = 2, 3
     video.imgs = glob.glob(f'imgs/2-1/*')
     dsize=(750, 500)
-elif option == "4x3": # 横長 2:1 できない
+elif option == "4x3": # 横長 2:1
     video.rows, video.cols = 3, 4
     video.imgs = glob.glob(f'imgs/2-1/*')
     dsize=(750, 500)
-elif option == "5x4": # 横長 2:1
+elif option == "5x4": # 横長 2:1 足りない
     video.rows, video.cols = 4, 5
     video.imgs = glob.glob(f'imgs/2-1/*')
     dsize=(750, 500)
-elif option == "2x3": # 縦長 1:2 処理の未実装
+elif option == "2x3": # 縦長 1:2
     video.rows, video.cols = 3, 2
+    video.imgs = glob.glob(f'imgs/1-2/*')
+    # dsize=(460, 900)
+    dsize=(358, 760)
+elif option == "3x4": # 縦長 1:2
+    video.rows, video.cols = 4, 3
     video.imgs = glob.glob(f'imgs/1-2/*')
     # dsize=(460, 900)
     dsize=(358, 760)
 
 
-# 未実装
-st.sidebar.radio(
+# 難易度
+level = st.sidebar.radio(
     label = "むずかしさ",
-    options = ["かんたん", "ふつう", "むずかしい"]
+    options = ["かんたん", "ふつう", "むずかしい"],
+    horizontal=True
 )
+# st.sidebar.text('かんたん:入れ替え ふつう:90度 むずかしい:90度180度回転')
 
-with open("マーカbig.pdf", "rb") as pdf_file:
-    PDFbyte = pdf_file.read()
+# st.sidebar.info('マーカー50番でのランダム抽選です')
 
-st.sidebar.download_button(
-    label="ARマーカー",
-    data=PDFbyte,
-    file_name="ARmarker.pdf",
-    mime='application/octet-stream')
-
+if (level == "かんたん"):
+    video.level = 0
+elif (level == "ふつう"):
+    video.level = 1
+elif (level == "むずかしい"):
+    video.level = 2
+else:
+    video.level = 0
 
 placeholder_che = st.empty()
 # 
-agree = st.sidebar.button('ARマーカーで画像のランダム抽選', key='q')
-
-if not agree:
-    randm_img = st.sidebar.button('単発ランダム抽選')
-    if randm_img:
-        try:
-            video.original_img = cv2.imread(random.choice(video.imgs))
-            video.original_img = cv2.resize(video.original_img, dsize=dsize)
-            # print(video.original_img)
-            # 元画像, 比較画像
-            video.img, video.comparison_img = video.imgCut(video.original_img, video.rows, video.cols)
-            # print(video.img)
-        except:
-            pass
-    else:
-        try:
-            video.original_img = cv2.imread(random.choice(video.imgs))
-            video.original_img = cv2.resize(video.original_img, dsize=dsize)
-            # 元画像, 比較画像
-            video.img, video.comparison_img = video.imgCut(video.original_img, video.rows, video.cols)
-        except:
-            pass
+# agree = st.sidebar.button('ARマーカーで画像のランダム抽選', key='q')
+randm_img = st.sidebar.button('画像抽選')
+if randm_img:
+    try:
+        video.original_img = cv2.imread(random.choice(video.imgs))
+        video.original_img = cv2.resize(video.original_img, dsize=dsize)
+        # print(video.original_img)
+        # 元画像, 比較画像
+        video.img, video.comparison_img = video.imgCut(video.original_img, video.rows, video.cols)
+        # print(video.img)
+    except:
+        pass
 else:
-    st.sidebar.button('OFF')
-    st.sidebar.info('マーカー50番でのランダム抽選です')
+    try:
+        video.original_img = cv2.imread(random.choice(video.imgs))
+        video.original_img = cv2.resize(video.original_img, dsize=dsize)
+        # 元画像, 比較画像
+        video.img, video.comparison_img = video.imgCut(video.original_img, video.rows, video.cols)
+    except:
+        pass
 
+dictionary = video.aruco.Dictionary_get(video.aruco.DICT_5X5_50)
 # カメラメイン処理
 def video_frame_callback(frame):
     global is_random_img
-    global agree
+    # global agree
+    global dictionary
     
     frame = frame.to_ndarray(format = 'bgr24')
     
@@ -119,7 +124,7 @@ def video_frame_callback(frame):
     # frame[0:h, 0:w] = self.original_img
     
     #　マーカの検出
-    dictionary = video.aruco.Dictionary_get(video.aruco.DICT_5X5_50)
+    # dictionary = video.aruco.Dictionary_get(video.aruco.DICT_5X5_50)
     # dictionary = video.aruco.getPredefinedDictionary(video.aruco.DICT_5X5_50)
     
     # corners:マーカの角 ids:マーカID
@@ -131,13 +136,13 @@ def video_frame_callback(frame):
     # マーカIDが存在するか
     if np.all(ids != None):
         # 特定のマーカーが読み込まれたら画像のチェンジ
-        if 10 in ids and agree:
-            video.original_img = cv2.imread(random.choice(video.imgs))
-            video.original_img = cv2.resize(video.original_img, dsize=(500, 500))
-            video.img, video.comparison_img = video.imgCut(video.original_img, video.rows, video.cols)
-            is_random_img = True
-        else:
-            is_random_img = False
+        # if 10 in ids and agree:
+        #     video.original_img = cv2.imread(random.choice(video.imgs))
+        #     video.original_img = cv2.resize(video.original_img, dsize=(500, 500))
+        #     video.img, video.comparison_img = video.imgCut(video.original_img, video.rows, video.cols)
+        #     is_random_img = True
+        # else:
+        #     is_random_img = False
             # cv2.imshow('img', original_img)
         
         # 検出されたマーカIDの数だけ繰り返す
@@ -183,6 +188,40 @@ def video_frame_callback(frame):
                     frame, frame2 = video.overlapImg(video.img[14], pts_dst, frame, frame2)
                 elif ids[i] == 15 and len(video.img) >= 15:
                     frame, frame2 = video.overlapImg(video.img[15], pts_dst, frame, frame2)
+                elif ids[i] == 16 and len(video.img) >= 16:
+                    frame, frame2 = video.overlapImg(video.img[16], pts_dst, frame, frame2)
+                elif ids[i] == 17 and len(video.img) >= 17:
+                    frame, frame2 = video.overlapImg(video.img[17], pts_dst, frame, frame2)
+                elif ids[i] == 18 and len(video.img) >= 18:
+                    frame, frame2 = video.overlapImg(video.img[18], pts_dst, frame, frame2)
+                elif ids[i] == 19 and len(video.img) >= 19:
+                    frame, frame2 = video.overlapImg(video.img[19], pts_dst, frame, frame2)
+                elif ids[i] == 20 and len(video.img) >= 20:
+                    frame, frame2 = video.overlapImg(video.img[20], pts_dst, frame, frame2)
+                elif ids[i] == 21 and len(video.img) >= 21:
+                    frame, frame2 = video.overlapImg(video.img[21], pts_dst, frame, frame2)
+                elif ids[i] == 22 and len(video.img) >= 22:
+                    frame, frame2 = video.overlapImg(video.img[22], pts_dst, frame, frame2)
+                elif ids[i] == 23 and len(video.img) >= 23:
+                    frame, frame2 = video.overlapImg(video.img[23], pts_dst, frame, frame2)
+                elif ids[i] == 24 and len(video.img) >= 24:
+                    frame, frame2 = video.overlapImg(video.img[24], pts_dst, frame, frame2)
+                elif ids[i] == 25 and len(video.img) >= 25:
+                    frame, frame2 = video.overlapImg(video.img[25], pts_dst, frame, frame2)
+                elif ids[i] == 26 and len(video.img) >= 26:
+                    frame, frame2 = video.overlapImg(video.img[26], pts_dst, frame, frame2)
+                elif ids[i] == 27 and len(video.img) >= 27:
+                    frame, frame2 = video.overlapImg(video.img[27], pts_dst, frame, frame2)
+                elif ids[i] == 28 and len(video.img) >= 28:
+                    frame, frame2 = video.overlapImg(video.img[28], pts_dst, frame, frame2)
+                elif ids[i] == 29 and len(video.img) >= 29:
+                    frame, frame2 = video.overlapImg(video.img[29], pts_dst, frame, frame2)
+                elif ids[i] == 30 and len(video.img) >= 30:
+                    frame, frame2 = video.overlapImg(video.img[30], pts_dst, frame, frame2)
+                elif ids[i] == 31 and len(video.img) >= 31:
+                    frame, frame2 = video.overlapImg(video.img[31], pts_dst, frame, frame2)
+                elif ids[i] == 32 and len(video.img) >= 32:
+                    frame, frame2 = video.overlapImg(video.img[32], pts_dst, frame, frame2)
             except:
                 pass
             
@@ -220,18 +259,60 @@ ctx = webrtc_streamer(
     
 placeholder = st.sidebar.empty()
 # 正しい画像の表示
-if agree and ctx.state.playing:
-    cou = 0
-    while agree:
-        # print(is_random_img)
-        if is_random_img:
-            cou += 1
-            placeholder.image(video.cv2pil(video.original_img), caption='元画像')
-        else:
-            placeholder.image(video.cv2pil(video.original_img), caption='元画像')
-        if not agree or cou > 500:
-            print('of')
-            agree = False
-            break
-else:
+if ctx.state.playing:
     placeholder.image(video.cv2pil(video.original_img), caption='元画像')
+    
+with open("マーカbig.pdf", "rb") as pdf_file:
+    PDFbyte = pdf_file.read()
+
+st.download_button(
+    label="ARマーカーダウンロード",
+    data=PDFbyte,
+    file_name="ARmarker.pdf",
+    mime='application/octet-stream')
+
+st.write(f"""
+# 遊び方
+
+### PC推奨
+
+以下のものを準備
+* ARマーカー(ARマーカーダウンロードをクリック)
+* カメラ
+
+`赤いstartボタン`を押すし、しばらく待ちます。
+
+カメラの映像が出力されます。
+
+マーカーの`marker-0`をかざすと画像の一部が表示されます。
+
+あとは、サイドバーから、パズルサイズを変更や、むずかしさの変更、画像抽選などして遊びます。
+
+## サイドバーについて
+
+### パズルサイズ 横x縦
+
+|サイズ|必要マーカー番号|
+|--|--|
+|2x2|0~3|
+|3x3|0~8|
+|4x4|0~15|
+|5x5|0~24|
+|3x2|0~5|
+|4x3|0~11|
+|5x4|0~19|
+|2x3|0~5|
+|3x4|0~11|
+
+### むずかしさ
+
+|むずかしさ|詳細|
+|--|--|
+|かんたん|入れ替え|
+|ふつう|入れ替え、90度回転|
+|むずかしい|入れ替え、90度回転&180度回転|
+
+### 画像抽選
+
+ボタンを押すとランダムに画像を変更できる
+""")
