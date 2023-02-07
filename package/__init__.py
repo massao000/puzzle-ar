4f697217ac4e401e2b0e51df20f74da7ba30d5e9
massao000 <onepiece.4224y@icloud.com>
2023-02-08 02:56:22 +0900

    in

--- package/__init__.py
+++ package/__init__.py
@@ -1,4 +1,4 @@
 # from package.marker import pil2cv, cv2pil, comparison, trimming, randomRotate, concat_tile, imgCut, overlapImg
-from package.marker import VideoProcessor
+from package.marker import VideoProcessor, pil2cv, cv2pil
 # from package.markerclass import VideoProcessor
 # from package.markerclass2 import VideoProcessor
